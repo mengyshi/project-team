d6cc86e770c1b882cb3f3a34e3c7250765e949be
王娜 <2969929592@qq.com>
2019-07-19 15:43:06 +0800

    配置vant样式文件

--- pro/src/main.js
+++ pro/src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
+import 'vant/lib/index.css';
 Vue.config.productionTip = false
 Vue.use(Vant)
 /* eslint-disable no-new */
