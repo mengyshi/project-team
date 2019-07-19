21a9a9eb6c5afa37b810adad11d0df3430662e8f
王娜 <2969929592@qq.com>
2019-07-19 14:40:34 +0800

    配置vuex--vant

--- pro/src/main.js
+++ pro/src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Vuex from 'vuex'
+import Vant from 'vant'
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
+Vue.use(Vant)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
