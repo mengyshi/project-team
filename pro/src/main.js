c68ef688ea9e29af7453fb69c081606a9b23a6c5
王娜 <2969929592@qq.com>
2019-07-19 14:59:13 +0800

    配置vuex路径store

--- pro/src/main.js
+++ pro/src/main.js
@@ -3,11 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import Vuex from 'vuex'
 import Vant from 'vant'
+import store from './store';
 Vue.config.productionTip = false
 Vue.use(Vuex)
 Vue.use(Vant)
+Vue.use(store)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
