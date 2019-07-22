73260499bed37bbea1beee7246d3dc4c8dcd88b0
mengyshi <3219145319@qq.com>
2019-07-22 11:37:20 +0800

    test

--- pro/src/router/index.js
+++ pro/src/router/index.js
@@ -9,10 +9,23 @@ import Comment from  '@/views/Comment'
 import WriteComment from  '@/views/WriteComment'
 import OwnPage from  '@/views/OwnPage'
 import Setting from  '@/views/Setting'
-
 import PersonalData from  '@/views/PersonalData'
 import AddressList from  '@/views/AddressList'
 import AddAddress from  '@/views/AddAddress'
+import EditAddress from  '@/views/EditAddress'
+import Collect from  '@/views/Collect'
+import UpdateInfo from  '@/views/UpdateInfo'
+// 更改个人信息组件
+import Username from  '@/views/personalInfo/Username'
+import TelPhone from  '@/views/personalInfo/TelPhone'
+import Mail from  '@/views/personalInfo/Mail'
+import Sex from  '@/views/personalInfo/Sex'
+import IdCard from  '@/views/personalInfo/IdCard'
+import City from  '@/views/personalInfo/City'
+import Birthday from  '@/views/personalInfo/Birthday'
+import Hometown from  '@/views/personalInfo/Hometown'
+
+
 
 import 'vant/lib/index.css'
 
@@ -54,6 +67,46 @@ export default new Router({
     },{
       path:"/addaddress",
       component:AddAddress
+    },{
+      path:"/editaddress",
+      component:EditAddress
+    },{
+      path:"/collect",
+      component:Collect,
+    },{
+      path:"/updateinfo",
+      component:UpdateInfo,
+      children:[
+        {
+          path:"/username",
+          component:Username,
+        },
+        {
+          path:"/telphone",
+          component:TelPhone,
+        },
+        {
+          path:"/hometown",
+          component:Hometown,
+        }, {
+          path:"/sex",
+          component:Sex,
+        },
+        {
+          path:"/city",
+          component:City,
+        },
+        {
+          path:"/birthday",
+          component:Birthday,
+        },
+         {
+          path:"/mail",
+          component:Mail,
+        },
+
+
+      ]
     }
 
   ]
