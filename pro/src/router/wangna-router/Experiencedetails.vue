ce0a1004ca9032092cd9d1b88bddc42c186b405f
baitwoWN <2969929592@qq.com>
2019-07-23 22:15:14 +0800

    美食店铺列表展示

--- pro/src/router/wangna-router/Experiencedetails.vue
+++ pro/src/router/wangna-router/Experiencedetails.vue
@@ -8,40 +8,74 @@
       <div class="hotelinfo">
         <van-image width="100%" height="100%" src="https://img.yzcdn.cn/vant/cat.jpeg" />
         <ul>
-          <li>店名</li>
-          <li>地点所在市</li>
-          <li>价位（每位价格）</li>
-          <li>主要经营（包括饮品、设施和零食）</li>
+          <li>{{list.shopname}}</li>
+          <li>{{list1.address}}</li>
+          <li>￥{{list.price}}/每人</li>
+          <li>{{list.detail}}</li>
           <li>店提供服务（简体中文和英文）名</li>
         </ul>
       </div>
-        <ul class="detail-con">
-          <li class="understing">
-            <span>认识一下Scarlett</span>
-            <van-image
-              width="52"
-              height="52"
-              src="https://img.yzcdn.cn/vant/cat.jpeg"
-              :round="true"
-            />
-          </li>
-          <li>我生长于深圳的一个法律世家，而是因受儒、释、道三种思的交叉感染，养成了7大爱好；琴/色/书/画/诗/酒/茶</li>
-          <li>
-              <span>体验内包括的东西</span>
-              <p>燃一款我最爱的沉香<br>品尝我沉迷的茶配点心<br>h喝到三种我精心挑选的茶</p>
-          </li>
-        </ul>
+      <ul class="detail-con">
+        <li class="understing">
+          <span>认识一下Scarlett</span>
+          <van-image width="52" height="52" src="https://img.yzcdn.cn/vant/cat.jpeg" :round="true" />
+        </li>
+        <li class="jieshao">我生长于深圳的一个法律世家，而是因受儒、释、道三种思的交叉感染，养成了7大爱好；琴/色/书/画/诗/酒/茶。
+        </li>
+        <li>
+          <span>活动内容</span>
+          <p>我会带你品尝我精心挑选的几款茶，教你用传统仪式感，以及独具深圳式创造力的方式喝茶、烹饪、品茶。</p>
+          <p>如果你是一枚茶界的小白，那就更棒了，在我的茶馆里面我会带你品尝我精心挑选的几款茶。</p>
+        </li>
+        <li>
+          <span>体验内包括的东西</span>
+          <p>燃一款我最爱的沉香</p>
+          <p>品尝我沉迷的茶配点心</p>
+          <p>喝到三种我精心挑选的茶</p>
+        </li>
+        <li>
+          <span>备注</span>
+          <p>我会带你品尝我精心挑选的几款茶，教你用传统仪式感，以及独具深圳式创造力的方式喝茶、烹饪、品茶。</p>
+          <p>如果你是一枚茶界的小白，那就更棒了，在我的茶馆里面我会带你品尝我精心挑选的几款茶。</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Experiencedetails",
-  methods: {
-      cancelBtn(){
-          this.$router.go(-1)
-      }
+  data() {
+    return {
+      list: [],
+      list1: []
+    };
   },
+  methods: {
+    cancelBtn() {
+      this.$router.go(-1);
+    }
+  },
+  mounted() {
+    let that = this;
+    axios({
+      method: "get",
+      url: "http://10.8.157.8:8080/food/shopDetail.do",
+      params: {
+        id: that.$route.query.id
+      }
+    })
+      .then(function(res) {
+        that.list = res.data.info;
+        that.list1 = res.data.info.deliciousDetail;
+        console.log(that.list);
+        console.log(that.list1);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
 };
 </script>
 <style lang="" scope="">
@@ -76,21 +110,27 @@ export default {
   margin-bottom: 10px;
 }
 .detail-con li {
-    padding: 10px 10px 20px 10px;
-    line-height: 30px;
-    border-bottom: 1px solid #ccc;
-     margin-bottom: 32px;
-     text-align: left;
+  padding: 10px 10px 20px 10px;
+  line-height: 30px;
+  border-bottom: 1px solid #ccc;
+  margin-bottom: 32px;
+  text-align: left;
 }
-.detail-con li span{
-    margin-bottom: 20px;
-    font-weight: 100;
+.detail-con li span {
+  margin-bottom: 20px;
+  font-weight: 100;
 }
 .detail-con .understing {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   height: 52px;
   border-bottom: 0;
+  padding: 20px;
 }
-
+.jieshao {
+  text-indent: 30px;
+}
+.detail-con li p {
+  text-indent: 30px;
+}
 </style>
