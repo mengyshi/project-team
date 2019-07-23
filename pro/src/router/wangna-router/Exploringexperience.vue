5076717bf79e2f2613f6807d1186e64a0f27f6f6
baitwoWN <2969929592@qq.com>
2019-07-23 22:17:30 +0800

    美食店铺列表展示

--- pro/src/router/wangna-router/Exploringexperience.vue
+++ pro/src/router/wangna-router/Exploringexperience.vue
@@ -2,47 +2,77 @@
   <div class="exploring">
     <!-- 返回 -->
     <p class="backBtn">
-      <van-icon name="arrow-left" @click="cancle" />
+      <van-icon size="30" name="arrow-left" @click="cancle" />
     </p>
     <!-- 列表 -->
     <div class="list-content">
       <div class="con-travel-story">
-        <van-grid :border="false" :column-num="2">
-          <van-grid-item>
-            <van-image src="https://img.yzcdn.cn/vant/apple-1.jpg" @click="projectInfo" />
-            <p>名称</p>
-            <p>描述</p>
-            <p>价格</p>
-            <van-rate v-model="value" />
-          </van-grid-item>
-          <van-grid-item>
-            <van-image src="https://img.yzcdn.cn/vant/apple-2.jpg" />
-            <p>名称</p>
-            <p>描述</p>
-            <p>价格</p>
-            <van-rate v-model="value" />
-          </van-grid-item>
-        </van-grid>
+        <ul class="shoplist">
+          <li v-for="item in list" :key="item.id">
+            <router-link
+              :to="{path:'Experiencedetails',name:'Experiencedetails',query:{id:item.id}}"
+            >
+              <van-image src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </router-link>
+            <p>{{item.shopname}}</p>
+            <p>{{item.detail}}</p>
+            <p>每人￥{{item.price}}</p>
+            <van-rate v-model="value" allow-half void-icon="star" void-color="#eee" />
+            <div class="Collection">
+              <van-icon name="like-o" @click="collectionpro" />
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Exploringexperience",
   data() {
     return {
-      value: 3
+      value: 0,
+      list: []
     };
   },
   methods: {
     cancle() {
       this.$router.go(-1);
     },
-    // 进入商品详情
-    projectInfo(){
-      this.$router.push('Experiencedetails')
+    collectionpro() {
+      axios({
+        method: "get",
+        url: "http://10.8.157.8:8080/food/collectDelicious.do",
+        params: {
+         uid:2,
+         deliciousid:1
+        }
+      })
+        .then(function(res) {
+          alert(res.data.info)
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     }
+  },
+  mounted() {
+    let that = this;
+    axios({
+      method: "get",
+      url: "http://10.8.157.8:8080/food/showShopdetail.do",
+      params: {
+        id: that.$route.query.id
+      }
+    })
+      .then(function(res) {
+        that.list = res.data.info;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   }
 };
 </script>
@@ -57,5 +87,35 @@ export default {
 .backBtn {
   text-align: left;
   text-indent: 10px;
+  margin-bottom: 6px;
+}
+/* 店铺列表 */
+.shoplist {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.shoplist li {
+  width: 50%;
+  padding: 10px;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+  position: relative;
+}
+.Collection {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+}
+.shoplist li img {
+  width: 100%;
+  height: 100%;
+  opacity: 0.6;
+}
+.shoplist li p {
+  text-align: left;
+  height: 20px;
+  margin-bottom: 3px;
+  color: #333;
 }
 </style>
